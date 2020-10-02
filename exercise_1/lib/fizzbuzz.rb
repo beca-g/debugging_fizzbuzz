41d9c1f0cf468d20dfffb0f29a703c31b2e1d714
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-02 12:39:12 +0100

    Solving a "The code doesn't do what the tests expect it to" error.
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Your branch is ahead of 'origin/master' by 2 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   lib/fizzbuzz.rb
    
    changing the code in line 6 <string.empty? ? string : number> to <string.empty? ?  number : string>

--- exercise_1/lib/fizzbuzz.rb
+++ exercise_1/lib/fizzbuzz.rb
@@ -3,12 +3,12 @@ class FizzBuzz
     string = ""
     string << "Fizz" if is_divisible_by?(3, number)
     string << "Bizz" if is_divisible_by?(5, number)
-    string.empty? ? string : number
+    string.empty? ?  number : string
   end
 
   private
 
   def is_divisible_by?(divisor, number)
-    number % divisor == 1
+    number % divisor == 0
   end
 end
