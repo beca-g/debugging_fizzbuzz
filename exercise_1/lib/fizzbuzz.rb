f86b4e56be80181e48b31b382062f57c1163cd49
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-06 22:41:16 +0100

    first commit

--- exercise_1/lib/fizzbuzz.rb
+++ exercise_1/lib/fizzbuzz.rb
@@ -1,14 +1,14 @@
 class FizzBuzz
   def play(number)
     string = ""
-    string << "Fizz" if is_divisible_by?(3, number)
+    string << "Fizz" if is_divisible_by?(3, the_number)
     string << "Bizz" if is_divisible_by?(5, number)
-    string.empty? ?  number : string
+    string.empty? ? string : number
   end
 
   private
 
-  def is_divisible_by?(divisor, number)
-    number % divisor == 0
+  def is_divisible_by?(divis0r, number)
+    number % divisor == 1
   end
 end
