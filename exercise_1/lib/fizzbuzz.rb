9d426b28706adead41289a618a01057bba428bed
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-06 23:02:39 +0100

    A bug in the code error. Final error solved All tests now pass
    Failures:
    
      1) FizzBuzz#play returns Buzz when a number is divisible by 5
         Failure/Error: expect(subject.play(5)).to eq "Buzz"
    
           expected: "Buzz"
                got: "Bizz"
    
           (compared using ==)
         # ./spec/fizzbuzz_spec.rb:18:in `block (3 levels) in <top (required)>'

--- exercise_1/lib/fizzbuzz.rb
+++ exercise_1/lib/fizzbuzz.rb
@@ -2,7 +2,7 @@ class FizzBuzz
   def play(number)
     string = ""
     string << "Fizz" if is_divisible_by?(3, number)
-    string << "Bizz" if is_divisible_by?(5, number)
+    string << "Buzz" if is_divisible_by?(5, number)
     string.empty? ?  number : string
   end
 
